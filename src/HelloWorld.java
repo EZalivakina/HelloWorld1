c1c5502944cb16e45355a20aeefafc57c34418b4
EZalivakina <gnomik-e-v@yandex.ru>
2019-02-20 21:02:21 +0300

    Задача №4: Отработка поразрядных операций Ввести с консоли число в бинарном формате. Перевести его в int и вывести на экран

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,13 @@
-public class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Hello, world!");
-    }
-}
+
+    import java.util.Scanner;
+    public class HelloWorld {
+            public static void main(String[] args) {
+                Scanner sc = new Scanner(System.in);
+                System.out.print("Введите число в бинарном формате: ");
+
+                    String i = sc.next();
+
+                    System.out.println(Integer.parseInt(i, 2));
+                    sc.close();
+            }
+        }
