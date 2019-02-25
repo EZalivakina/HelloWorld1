2f326b39c707666d14e485b6d339682bf72c0ae9
EZalivakina <gnomik-e-v@yandex.ru>
2019-02-25 21:20:18 +0300

    Задача №8: Цикличный вывод данных на экран Необходимо с помощью цикла вывести на экран числа от 1 до 50.

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,28 +3,12 @@
     public class HelloWorld {
         public static void main(String[] args) {
             Scanner sc = new Scanner(System.in);
-            System.out.print("Введите число: ");
-            int number = new Scanner(System.in).nextInt();
-            int i=1;
-            System.out.println( i*number);
-            i++;
-            System.out.println( i*number);
-            i++;
-            System.out.println( i*number);
-            i++;
-            System.out.println( i*number);
-            i++;
-            System.out.println( i*number);
-            i++;
-            System.out.println( i*number);
-            i++;
-            System.out.println( i*number);
-            i++;
-            System.out.println( i*number);
-            i++;
-            System.out.println( i*number);
-
-            sc.close();
+            for (int i = 1; i <= 50; i++) {
+                System.out.println(i);
+                sc.close();
             }
+
         }
+    }
+
 
