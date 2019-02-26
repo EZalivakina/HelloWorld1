62b0d1ee42734ebe2e6d03a9a92e7750fd55c9ef
EZalivakina <gnomik-e-v@yandex.ru>
2019-02-26 19:20:54 +0300

    Задача №7: Условие вывода данных Задание Необходимо написать программу, где бы пользователю предлагалось ввести с клавиатуры число в переменную T. В программе должны присутствовать константы X, Y, Z. Программа должна сравнивать введенное значение с клавиатуры со значением констант и вывести на экран сообщение "Данное значение имеется в константах" если T=X или Y или Z. Если переменная отличается от констант, нужно вывести оповещение "Такой константы нет!"

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -4,21 +4,18 @@
         public static void main(String[] args) {
             Scanner sc = new Scanner(System.in);
             System.out.print("Введите число: ");
-            double x = new Scanner(System.in).nextDouble();
-            System.out.print("Введите число: ");
-            double y = new Scanner(System.in).nextDouble();
-            System.out.print("Введите число: ");
-            double z = new Scanner(System.in).nextDouble();
-            double avsum = (x+y+z)/3;
-            System.out.println(avsum);
-            int sum = (int)(avsum /= 2);
-            if (sum>3) {
-                System.out.println("Программа выполнена корректно");
+            double t = new Scanner(System.in).nextDouble();
+            double x = 10;
+            double y = 5;
+            double z = 9.5;
+            if (t==x | t==y | t==z) {
+                System.out.println("Данное значение имеется в константах");
             }
+            else {
+                System.out.println("Такой константы нет!");
+                }
                  sc.close();
             }
-
-
         }
 
 
