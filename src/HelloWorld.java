e0bde67a0859caf2b2c8be1619264dc2bb71a11f
EZalivakina <gnomik-e-v@yandex.ru>
2019-02-26 20:19:43 +0300

    Задание №10: Ввод/вывод данных для матрицы Произведите ввод данных с клавиатуры в матрицу, а после этого произведите вывод первой строки матрицы на экран, где каждый элемент умножается на 3.

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,17 +3,21 @@
     public class HelloWorld {
         public static void main(String[] args) {
             Scanner sc = new Scanner(System.in);
-            System.out.print("Введите размер массива: ");
+            System.out.print("Введите количество строк: ");
+            int  s = new Scanner(System.in).nextInt();
+            System.out.print("Введите количество столбцов: ");
             int  t = new Scanner(System.in).nextInt();
-            double  sample[] = new double [t];
-            int i;
-            for (i=0; i<t; i++) {
-                System.out.print("Введите число в массив: ");
-                double  num = new Scanner(System.in).nextDouble();
-                sample[i] = num;
+            double  sample[][] = new double [s][t];
+            int i,j;
+            for (i=0; i<s; ++i) {
+                for (j = 0; j < t; ++j) {
+                    System.out.print("Введите число в матрицу: "+" "+i+" "+j+": ");
+                    double num = new Scanner(System.in).nextDouble();
+                    sample[i][j] = num;
+                }
             }
-            for (i=0; i<t; i++)
-                System.out.println("Элемент[" + i + "]:" + sample[i]*2);
+            for (j=0; j<t; j++)
+                System.out.println("Элемент[0][" + j + "]:" + sample[0][j]*3);
 
                  sc.close();
             }
