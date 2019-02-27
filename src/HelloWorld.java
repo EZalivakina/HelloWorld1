c39b020fd8858e785c90969994b7e2c41599695f
EZalivakina <gnomik-e-v@yandex.ru>
2019-02-27 19:57:28 +0300

    Задача №11: Ввод строки с клавиатуры Задание Необходимо произвести ввод строки с клавиатуры и записать в строковую переменную. Результат вывести на экран.

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,23 +3,10 @@
     public class HelloWorld {
         public static void main(String[] args) {
             Scanner sc = new Scanner(System.in);
-            System.out.print("Введите количество строк: ");
-            int  s = new Scanner(System.in).nextInt();
-            System.out.print("Введите количество столбцов: ");
-            int  t = new Scanner(System.in).nextInt();
-            double  sample[][] = new double [s][t];
-            int i,j;
-            for (i=0; i<s; ++i) {
-                for (j = 0; j < t; ++j) {
-                    System.out.print("Введите число в матрицу: "+" "+i+" "+j+": ");
-                    double num = new Scanner(System.in).nextDouble();
-                    sample[i][j] = num;
-                }
-            }
-            for (j=0; j<t; j++)
-                System.out.println("Элемент[0][" + j + "]:" + sample[0][j]*3);
-
-                 sc.close();
+            System.out.print("Введите текст: ");
+            String   s = new Scanner(System.in).nextLine();
+            System.out.println("s = "+s);
+            sc.close();
             }
         }
 
