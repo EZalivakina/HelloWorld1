bb8f2e713d9d083c22c51ad2eca1fedf2e477d8d
EZalivakina <gnomik-e-v@yandex.ru>
2019-03-05 20:16:23 +0300

    Задача №14: Преобразование типов Необходимо написать программу, которая будет реализовывать следующие действия: 1. Ввод числа с клавиатуры и запись его в строковую переменную S
    2. Конвертация строковой переменной S в числовую переменную X типа int
    
    3. Конвертация числа X типа int в число Y типа double
    
    Все три числа разных типов необходимо в столбик вывести на экран

---
+++ src/type.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class type {
+    public static void main(String[] args) {
+        System.out.print("Введите число: ");
+        String S = new Scanner(System.in).nextLine();
+        int X = Integer.parseInt(S);
+        double Y = X;
+        System.out.println("Строковый тип:"+S);
+        System.out.println("Тип INT:"+X);
+        System.out.println("Тип double:"+Y);
+    }
+
+}
